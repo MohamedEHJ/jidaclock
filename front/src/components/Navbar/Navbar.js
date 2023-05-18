a960cbfc8bb7bd8b3e7b6449919c8295b028b3a2
Mohamed EL HAJOUI <elhajoui.mohamed@outlook.fr>
2023-05-18 17:22:51 +0200

    Edit unescaped char

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -33,7 +33,6 @@ export default class Navbar extends Component {
               d="M16.143 2l5.857 5.858v8.284l-5.857 5.858h-8.286l-5.857-5.858v-8.284l5.857-5.858h8.286zm.828-2h-9.942l-7.029 7.029v9.941l7.029 7.03h9.941l7.03-7.029v-9.942l-7.029-7.029zm-6.471 6h3l-1 8h-1l-1-8zm1.5 12.25c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z"
             />
           </svg>
-          {/* <span>BESOIN D'AIDE</span> */}
           <span>
             BESOIN D&apos;AIDE: <span className="font-bold">06.72.82.98.77</span>
           </span>
@@ -44,7 +43,7 @@ export default class Navbar extends Component {
             <div className="absolute bg-white text-black rounded-md p-4">
               <div className="text-xl font-semibold mb-2">Signaler un problème</div>
               <ul>
-                <li>Envoyez-moi un message WhatsApp au : <p className='font-bold'>06 72 82 98 70</p> avec si possible une photo de l'écran.</li>
+                <li>Envoyez-moi un message WhatsApp au : <p className='font-bold'>06 72 82 98 70</p> avec si possible une photo de l&apos;écran.</li>
               </ul>
               <button className="mt-4 text-white bg-red-500 py-2 px-4 rounded-md" onClick={this.handleClosePopup}>
                 OK
